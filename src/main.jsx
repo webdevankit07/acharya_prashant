2718a4df0b780cd5b9ad93a761159e4828e2b85c
Ankit Yadav <ankityadav.webdev@gmail.com>
2023-10-26 07:51:58 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { Provider } from 'react-redux';
-import Store from './store/store.jsx';
+import Store from './store/Store.jsx';
 import { ThemeProvider } from 'styled-components';
 
 const theme = {
